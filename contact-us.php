be1f440f5ddba3f015a6d19faa88812945d93db0
BP <bp@BPs-MacBook-Air.local>
2026-06-06 17:26:48 +0530

    remove js validation class from contact-us

--- contact-us.php
+++ contact-us.php
@@ -51,8 +51,8 @@ require_once 'header.php';
                 </div>
             </div>
 
-            <div class="contact-form">
-                <form accept-charset="UTF-8" action="https://app.formester.com/forms/PeoZoWPaY/submissions" method="POST" class="appointment-form">
+            <div class="">
+                <form accept-charset="UTF-8" action="https://app.formester.com/forms/PeoZoWPaY/submissions" method="POST">
                     <div class="form-group">
                         <label for="name">Your Name *</label>
                         <input type="text" id="name" name="name" required>
